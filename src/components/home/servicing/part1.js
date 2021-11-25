2217edf3e1f5d69802f53055d8677ee590cd2a26
unknown <khanmashrur03@gmail.com>
2021-11-25 16:00:46 +0600

    servicing revised

--- src/components/home/servicing/part1.js
+++ src/components/home/servicing/part1.js
@@ -2,41 +2,39 @@ import img1 from './images/img1.png'
 import img2 from './images/img2.png'
 import img3 from './images/img3.png'
 import img4 from './images/img4.png'
+import leftImg from './images/forklift.png'
 
 const ServicingPart1 = () => {
     return(
         <div className="container-fluid servicingPart1">
             <div className="row">
                 <div className="col-lg-6 col-md-12 col-sm-12">
-
+                    <img src={leftImg} className="servicingPart1_img"/>
                 </div>
                 <div className="col-lg-6 col-md-12 col-sm-12 rightDivSerivinfPart1">
-                       <p className="rightDivSerivinfPart1para">AKIJ Motors solely believes in total customer satisfaction with "Trust &amp; Mutual Benefit". 
-                           With this particular view, the Akij Motors introduces a One Stop Automobile Solution in Bangladesh and proudly announces the affiliation with some of world's renowned automobile &amp; garage equipment manufacturers.
-                      </p>
                     <div className="grid-container-servicing">
                             <div>
                                 <img src={img1}/>
-                                <h6>Super Fast</h6>
-                                <p>Confidence in Motion. Creating a Higher Standard. Creative technologie.</p>
+                                <h6>Powerful engines, strong chassis</h6>
+                                <p>We use engines which follow garmany technologies. We also have original ichijo engines(japan), Nishan petrol pump engines(japan), single weichai engines e.t.c. </p>
                             </div>
 
                             <div>
                                 <img src={img2}/>
-                                <h6>Air conditioning</h6>
-                                <p>Clean air for your breathing. Just the right temparature. </p>
+                                <h6>Fuel efficiency &amp; eco-friendly</h6>
+                                <p>Our products cost less fuel to run the vehicle. So it burns less diesel or gas. what reduces pollution and maintains a healthy environment. </p>
                             </div>
 
                             <div>
                                 <img src={img3}/>
-                                <h6>Transmission</h6>
-                                <p>GRACE, SPACE, PACE </p>
-                            </div>
+                                <h6>Low maintenance cost</h6>
+                                <p>Our vehicles costes less fuel so it reduces maintenance cost. On the other hand, spare parts are easily found on our site. </p>
+                           </div>
 
                             <div>
                                 <img src={img4}/>
-                                <h6>Dealership</h6>
-                                <p>Affordability at your fingertip. </p>
+                                <h6>Easy payment plan</h6>
+                                <p>We have facilities to provide payment at a time or customer-friendly installment.. </p>
                             </div>
                     </div>
                 </div>
