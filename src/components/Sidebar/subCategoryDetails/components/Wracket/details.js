4e6ac2189d47f33c9cc7a4448acceaafdf0a219b
unknown <khanmashrur03@gmail.com>
2021-12-08 15:45:40 +0600

    font

--- src/components/Sidebar/subCategoryDetails/components/Wracket/details.js
+++ src/components/Sidebar/subCategoryDetails/components/Wracket/details.js
@@ -64,140 +64,140 @@ const Details_Wracker  = () => {
                 <div className="right__spc">
                   {/* add this portion if new specification is added*/}
                   <div className="even__content">
-                                      <h3>Outline size(L×W×H) (mm)</h3>
-                                      <h3> 9280×2495×3120</h3>
+                                      <h6>Outline size(L×W×H) (mm)</h6>
+                                      <h6> 9280×2495×3120</h6>
                                     </div>
                   {/* add this portion if new specification is added*/}
                   <div className="even__content">
-                    <h3>Rating mass(kg) / Empty mass(Kg)</h3>
-                    <h3>14370</h3>
+                    <h6>Rating mass(kg) / Empty mass(Kg)</h6>
+                    <h6>14370</h6>
                   </div>
                   <div className="even__content">
-                    <h3> Full mass(kg)</h3>
-                    <h3>16000</h3>
+                    <h6> Full mass(kg)</h6>
+                    <h6>16000</h6>
                   </div>
                   <div className="even__content">
-                    <h3>Emission standard</h3>
-                    <h3> 	
+                    <h6>Emission standard</h6>
+                    <h6> 	
 
-                      Euro 2   </h3>
+                      Euro 2   </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Engine</h3>
-                    <h3>WD615.62 ((weichai)  </h3>
+                    <h6>Engine</h6>
+                    <h6>WD615.62 ((weichai)  </h6>
                   </div>
                   
                   
                   <div className="even__content">
-                    <h3>Type</h3>
-                    <h3>6 Cylinder Diesel engine   </h3>
+                    <h6>Type</h6>
+                    <h6>6 Cylinder Diesel engine   </h6>
                   </div>
                   
                   <div className="even__content">
-                    <h3>Power/displacement(KW/ML)</h3>
-                    <h3> 9726 </h3>
+                    <h6>Power/displacement(KW/ML)</h6>
+                    <h6> 9726 </h6>
                   </div>
                   
                   <div className="even__content">
-                    <h3>Wheelbase(mm)</h3>
-                    <h3>  	
+                    <h6>Wheelbase(mm)</h6>
+                    <h6>  	
 
-                      5600 </h3>
+                      5600 </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Axle load(kg) F/R Front/ Rear</h3>
-                    <h3> 7000/13500</h3>
+                    <h6>Axle load(kg) F/R Front/ Rear</h6>
+                    <h6> 7000/13500</h6>
                   </div>
                   <div className="even__content">
-                    <h3>Tire</h3>
-                    <h3> 11.00-20,11.00R20</h3>
+                    <h6>Tire</h6>
+                    <h6> 11.00-20,11.00R20</h6>
                   </div>
                   <div className="even__content">
-                    <h3>Max speed (km/h)</h3>
-                    <h3> 	
+                    <h6>Max speed (km/h)</h6>
+                    <h6> 	
 
-                      90 </h3>
+                      90 </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Front overhang(mm)</h3>
-                    <h3> 1560</h3>
+                    <h6>Front overhang(mm)</h6>
+                    <h6> 1560</h6>
                   </div>
                   <div className="even__content">
-                    <h3>Rear
+                    <h6>Rear
 
-                      overhang(mm)</h3>
-                    <h3>
-                      2120 </h3>
+                      overhang(mm)</h6>
+                    <h6>
+                      2120 </h6>
                   </div>
                   <div className="even__content">
-                    <h3> Front track(mm)</h3>
-                    <h3>1958,1939</h3>
+                    <h6> Front track(mm)</h6>
+                    <h6>1958,1939</h6>
                   </div>
                   <div className="even__content">
-                    <h3>Rear track(mm)</h3>
-                    <h3>1800 </h3>
+                    <h6>Rear track(mm)</h6>
+                    <h6>1800 </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Approach angel(°)</h3>
-                    <h3>15  </h3>
+                    <h6>Approach angel(°)</h6>
+                    <h6>15  </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Departure angle(°)</h3>
-                    <h3>    	
+                    <h6>Departure angle(°)</h6>
+                    <h6>    	
 
-                      20 </h3>
+                      20 </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Steering</h3>
-                    <h3>  Power steering</h3>
+                    <h6>Steering</h6>
+                    <h6>  Power steering</h6>
                   </div>
                   
                   <div className="even__content">
-                    <h3>Brake</h3>
-                    <h3>   Air brake  </h3>
+                    <h6>Brake</h6>
+                    <h6>   Air brake  </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Cabin</h3>
-                    <h3>Straight head ,single row with bed</h3>
+                    <h6>Cabin</h6>
+                    <h6>Straight head ,single row with bed</h6>
                   </div>
                   <div className="even__content">
-                    <h3>Rating lifting capacity(kg)</h3>
-                    <h3> 1500</h3>
+                    <h6>Rating lifting capacity(kg)</h6>
+                    <h6> 1500</h6>
                   </div>
                   <div className="even__content">
-                    <h3>Maximum crane height(mm)</h3>
-                    <h3> 	5000  </h3>
+                    <h6>Maximum crane height(mm)</h6>
+                    <h6> 	5000  </h6>
                   </div>
                 
                   <div className="even__content">
-                    <h3>Bracket arm extend-retract distance (mm)</h3>
-                    <h3> 700</h3>
+                    <h6>Bracket arm extend-retract distance (mm)</h6>
+                    <h6> 700</h6>
                   </div>
                   
                   <div className="even__content">
-                    <h3>Crane arm extend-retract distance (mm) </h3>
-                    <h3> 3000 </h3>
+                    <h6>Crane arm extend-retract distance (mm) </h6>
+                    <h6> 3000 </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Bracket arm maximum length(mm) </h3>
-                    <h3> 3100 </h3>
+                    <h6>Bracket arm maximum length(mm) </h6>
+                    <h6> 3100 </h6>
                   </div>
                   
                   <div className="even__content">
-                    <h3>Maximum towing speed(km/h)</h3>
-                    <h3>  	
+                    <h6>Maximum towing speed(km/h)</h6>
+                    <h6>  	
 
-                      40 </h3>
+                      40 </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Maximum crane capacity(kg)</h3>
-                    <h3>8000×2 NO.</h3>
+                    <h6>Maximum crane capacity(kg)</h6>
+                    <h6>8000×2 NO.</h6>
                   </div>
                   
                 
                   <div className="even__content">
-                    <h3>Support leg</h3>
-                    <h3> 2</h3>
+                    <h6>Support leg</h6>
+                    <h6> 2</h6>
                   </div>
                   
                   
@@ -205,26 +205,26 @@ const Details_Wracker  = () => {
                 
                   
                   <div className="even__content">
-                    <h3>Rating towing capacity(kg)</h3>
-                    <h3>  8000×2 </h3>
+                    <h6>Rating towing capacity(kg)</h6>
+                    <h6>  8000×2 </h6>
                   </div>
                   
                   <div className="even__content">
-                    <h3>Leg span(mm)</h3>
-                    <h3> 800 </h3>
+                    <h6>Leg span(mm)</h6>
+                    <h6> 800 </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Steel wire rope length(m)</h3>
-                    <h3>  40 </h3>
+                    <h6>Steel wire rope length(m)</h6>
+                    <h6>  40 </h6>
                   </div>
                   
                   <div className="even__content">
-                    <h3>Maximum support strength(KN)</h3>
-                    <h3>120 </h3>
+                    <h6>Maximum support strength(KN)</h6>
+                    <h6>120 </h6>
                   </div>
                   <div className="even__content">
-                    <h3>Wire rope speed(m/min)</h3>
-                    <h3>8 </h3>
+                    <h6>Wire rope speed(m/min)</h6>
+                    <h6>8 </h6>
                   </div>
 
 
@@ -246,10 +246,10 @@ const Details_Wracker  = () => {
             Bangladesh, powered by "Akij Motors" &amp; "Car for all" (সবার জন্য
             গাড়ী). Read More...
           </p>
-          <h3 className="mt-5 btm__item">
+          <h6 className="mt-5 btm__item">
             *Please be informed "AKIJ Motors" has every rights to change the
             content without any prior notice.
-          </h3>
+          </h6>
         </div>
       </div>
     </>
